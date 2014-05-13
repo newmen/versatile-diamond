544d9727c076debcc29092aad528e5eefdc04a76
Gleb Averchuk <altermn@gmail.com>
2014-05-13 21:08:49 +0400

    fix atoms graph generator

--- analyzer/lib/generators/atoms_graph_generator.rb
+++ analyzer/lib/generators/atoms_graph_generator.rb
@@ -3,13 +3,6 @@ module VersatileDiamond
 
     # Provides methods for drawing atom properties and dependencies between them
     module AtomsGraphGenerator
-
-      ATOM_COLOR = 'darkgreen'
-      RELEVANTS_ATOM_COLOR = 'lightblue'
-      SAME_INCOHERENT_COLOR = 'gold'
-
-      TRANSFER_COLOR = 'green'
-
     private
 
       # Draws spec nodes and dependencies from classified atoms
@@ -17,7 +10,7 @@ module VersatileDiamond
       def atoms_of_base_specs(**params)
         draw_base_specs(base_surface_specs, params)
         base_surface_specs.each do |spec|
-          draw_atoms_for(spec, spec.parents, BASE_SPEC_COLOR)
+          draw_atoms_for(spec, SpeciesGraphGenerator::BASE_SPEC_COLOR)
         end
       end
 
@@ -26,7 +19,7 @@ module VersatileDiamond
       def atoms_of_specific_specs(**params)
         draw_specific_specs(specific_surface_specs, params)
         specific_surface_specs.each do |spec|
-          draw_atoms_for(spec, spec.parent, SPECIFIC_SPEC_COLOR)
+          draw_atoms_for(spec, SpeciesGraphGenerator::SPECIFIC_SPEC_COLOR)
         end
       end
 
@@ -34,69 +27,30 @@ module VersatileDiamond
       def atoms_for_termination_specs
         draw_termination_specs
         termination_specs.each do |spec|
-          draw_atoms_for(spec, nil, TERMINATION_SPEC_COLOR)
+          draw_atoms_for(spec, SpeciesGraphGenerator::TERMINATION_SPEC_COLOR)
         end
       end
 
       # Draw atoms for passed spec with edges from spec to each atom with
       # passed color
       #
-      # @param [Spec | SpecificSpec] spec the spec atoms of which will be shown
-      # @param [Spec | SpecificSpec] parent don't draw atoms same as parent
+      # @param [DependentSpec | Specresidual] spec  atoms of which will be shown
       # @param [String] color the color of edges
-      def draw_atoms_for(spec, parent, color)
-        classification = classifier.classify(spec, without: parent)
-        draw_atoms(classification, @spec_to_node[spec.name], color)
+      def draw_atoms_for(spec, color)
+        classification = classifier.classify(spec)
+        draw_atoms(classification, @spec_to_node[spec.name])  do |e|
+          e.color = color
+        end
       end
 
       # Draws classified atoms and their dependencies from spec
       # @param [Hash] classification the classified atoms hash
-      # @param [Node] node the node of spec which belongs to atoms from hash
-      def draw_atoms(classification, node = nil, color = nil)
-        classification.each do |index, (image, _)|
+      # @yeild [Node | Edge] do setup of graph entities
+      def draw_atoms(classification, parent_node, &setup_block)
+        classification.each do |index, (image, edges_num)|
           add_atom_node(index, image)
-
-          next unless node
-          @graph.add_edges(node, get_atom_node(index)).set do |e|
-            e.color = color
-          end
-        end
-      end
-
-      # Draws transitions between atom properties by reactions
-      def draw_atom_transitions
-        cache = {}
-        spec_reactions.each do |reaction|
-          reaction.changes.each do |spec_atoms|
-            next if spec_atoms.map(&:first).any?(&:gas?)
-
-            indexes = spec_atoms.map do |spec_atom|
-              classifier.index(*spec_atom)
-            end
-
-            cache[indexes[0]] ||= Set.new
-            next if cache[indexes[0]].include?(indexes[1])
-            cache[indexes[0]] << indexes[1]
-
-            nodes = indexes.map { |i| get_atom_node(i) }
-            @graph.add_edges(*nodes).set do |e|
-              e.color = TRANSFER_COLOR
-            end
-          end
-        end
-
-        transitions_between_in(classifier.actives_to_deactives)
-        transitions_between_in(classifier.deactives_to_actives)
-      end
-
-      def transitions_between_in(one_face_of_mirror)
-        one_face_of_mirror.each_with_index do |to, from|
-          next if to == from
-          to_node = get_atom_node(to)
-          from_node = get_atom_node(from)
-
-          @graph.add_edges(from_node, to_node).set do |e|
-            e.color = TRANSFER_COLOR
+          edges_num.times do
+            @graph.add_edges(parent_node, get_atom_node(index)).set(&setup_block)
           end
         end
       end
--- analyzer/lib/generators/atoms_species_tree.rb
+++ analyzer/lib/generators/atoms_species_tree.rb
@@ -3,31 +3,38 @@ module VersatileDiamond
 
     # Provides methods for drawing atom properties and dependencies between them
     class AtomsSpeciesTree < Tree
-      include AtomsGraphGenerator
       include SpeciesGraphGenerator
+      include AtomsGraphGenerator
+      include AtomDependenciesGenerator
 
       # Generates a graph image file
       # @option [Boolean] :base_specs species will be shown an graph or not
       # @option [Boolean] :spec_specs specific species will be shown an graph or not
       # @option [Boolean] :no_includes are includes not will be shown at graph or not
-      # @option [Boolean] :no_transitions transitions between atoms not will be
-      #   shown or not
       def generate(base_specs: false, spec_specs: false, term_specs: false,
-        no_includes: false, no_transitions: false)
+        no_includes: false)
 
         if base_specs || spec_specs || term_specs
-          draw_base_specs(no_includes: no_includes) if base_specs
-          draw_specific_specs(no_includes: no_includes) if spec_specs
-          draw_termination_specs if term_specs
+          atoms_of_base_specs(no_includes: no_includes) if base_specs
+          atoms_of_specific_specs(no_includes: no_includes) if spec_specs
+          atoms_for_termination_specs if term_specs
         else
-          surface_specs.each { |s| draw_atoms(classifier.classify(s)) }
+          classify_atoms_of_specs(surface_specs)
         end
 
         draw_atom_dependencies unless no_includes
-        draw_atom_transitions unless no_transitions
 
         generate_graph
       end
+
+    private
+
+      # Do classification of atoms of each of passed species
+      # @param [Array] specs the array of analyzing species
+      def classify_atoms_of_specs(specs)
+        specs.each { |s| classifier.classify(s) }
+      end
+
     end
 
   end
