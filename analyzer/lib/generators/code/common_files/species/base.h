02b3f24b1951dd82c85a93885e2da1be3085b19e
Gleb Averchuk <altermn@gmail.com>
2014-05-31 15:40:02 +0400

    add engine code common files

---
+++ analyzer/lib/generators/code/common_files/species/base.h
@@ -0,0 +1,59 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <species/localable_role.h>
+using namespace vd;
+
+#include "overall.h"
+
+template <class B, ushort ST, ushort USED_ATOMS_NUM>
+class Base : public Overall<B, ST>
+{
+    typedef Overall<B, ST> ParentType;
+
+public:
+    Atom *anchor() const override { return this->atom(indexes()[0]); }
+
+    void store() override;
+    void remove() override;
+
+protected:
+    template <class... Args> Base(Args... args) : ParentType(args...) {}
+
+    virtual const ushort *indexes() const = 0;
+    virtual const ushort *roles() const = 0;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+template <class B, ushort ST, ushort USED_ATOMS_NUM>
+void Base<B, ST, USED_ATOMS_NUM>::store()
+{
+    const ushort *idxs = this->indexes();
+    const ushort *rls = this->roles();
+
+    for (uint i = 0; i < USED_ATOMS_NUM; ++i)
+    {
+        this->atom(idxs[i])->describe(rls[i], this);
+    }
+
+    ParentType::store();
+}
+
+template <class B, ushort ST, ushort USED_ATOMS_NUM>
+void Base<B, ST, USED_ATOMS_NUM>::remove()
+{
+    if (this->isMarked()) return;
+
+    const ushort *idxs = this->indexes();
+    const ushort *rls = this->roles();
+
+    for (uint i = 0; i < USED_ATOMS_NUM; ++i)
+    {
+        this->atom(idxs[i])->forget(rls[i], this);
+    }
+
+    ParentType::remove();
+}
+
+#endif // BASE_H
