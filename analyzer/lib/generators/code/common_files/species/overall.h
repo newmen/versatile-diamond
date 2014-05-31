30d279f1fc5e5958642302095c5be59ef27acc17
Gleb Averchuk <altermn@gmail.com>
2014-05-31 15:40:02 +0400

    add engine code common files

---
+++ analyzer/lib/generators/code/common_files/species/overall.h
@@ -0,0 +1,44 @@
+#ifndef OVERALL_H
+#define OVERALL_H
+
+#include <species/base_spec.h>
+#include <species/source_spec.h>
+#include <species/dependent_spec.h>
+#include <species/parent_spec.h>
+#include <species/additional_atoms_wrapper.h>
+#include <species/atom_shift_wrapper.h>
+#include <species/atoms_swap_wrapper.h>
+#include <tools/typed.h>
+using namespace vd;
+
+#include "../phases/diamond_atoms_iterator.h"
+#include "../handbook.h"
+
+template <class B, ushort ST>
+class Overall : public Typed<B, ST>, public DiamondAtomsIterator
+{
+    typedef Typed<B, ST> ParentType;
+
+    bool _markedForRemove = false;
+
+public:
+    void remove() override;
+
+protected:
+    template <class... Args> Overall(Args... args) : ParentType(args...) {}
+
+    bool isMarked() const { return _markedForRemove; }
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+template <class B, ushort ST>
+void Overall<B, ST>::remove()
+{
+    _markedForRemove = true;
+
+    ParentType::remove();
+    Handbook::scavenger().markSpec(this);
+}
+
+#endif // OVERALL_H
