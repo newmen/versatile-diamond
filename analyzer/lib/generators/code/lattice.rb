632e8ca1d6cbd18481c38164f01449c9207de807
Gleb Averchuk <altermn@gmail.com>
2014-06-20 15:30:31 +0400

    add source file copier and apply src directory for all generated source files

--- analyzer/lib/generators/code/lattice.rb
+++ analyzer/lib/generators/code/lattice.rb
@@ -7,12 +7,14 @@ module VersatileDiamond
       # Creates Lattice class
       class Lattice < CppClass
         include PolynameClass
+        extend SourceFileCopier
         extend Forwardable
 
         def_delegators :instance, :default_surface_height
+        copy_source :crystal_properties, :relations
 
         # Initializes by concept lattice
-        # @param [Concepts::Lattice] lattice as element of Mendeleev's table
+        # @param [Concepts::Lattice] lattice the target concept
         # @param [Organizers::AtomClassifier] classifier of atom properties
         def initialize(lattice, classifier)
           @lattice = lattice
@@ -37,49 +39,6 @@ module VersatileDiamond
       private
 
         def_delegator :@lattice, :instance
-
-        %w(crystal_properties relations).each do |name|
-          name_method = :"#{name}_name"
-          file_name_method = :"#{name}_file_name"
-
-          # The name of lattice #{name} code instance
-          # @return [String] the #{name} underscored instance name
-          define_method(name_method) do
-            "#{class_name.underscore}_#{name}"
-          end
-
-          # The class name of lattice #{name} code instance
-          # @return [String] the class name of used #{name}
-          define_method(:"#{name}_class_name") do
-            send(name_method).classify
-          end
-
-          # The file name of lattice #{name} code instance
-          # @return [String] the file name of used #{name}
-          define_method(file_name_method) do
-            "#{send(name_method)}.h"
-          end
-
-          # The source file path to #{name} header file
-          # @return [Pathname] the path to source #{name} file
-          define_method(:"src_#{name}_file_path") do
-            template_dir + send(file_name_method)
-          end
-
-          # The destination file path to #{name} header file
-          # @param [String] root_dir the directory of generation results
-          # @return [Pathname] the path to result #{name} file
-          define_method(:"dst_#{name}_file_path") do |root_dir|
-            out_dir(root_dir) + send(file_name_method)
-          end
-
-          # Copy the #{name} header file to result source dir
-          # @param [String] root_dir the directory of generation results
-          define_method(:"cp_#{name}_file_path") do |root_dir|
-            FileUtils.cp(
-              send(:"src_#{name}_file_path"), send(:"dst_#{name}_file_path", root_dir))
-          end
-        end
 
         %w(major surface).each do |name|
           # Finds index of atom properties that correspond to #{name} atom instance of
