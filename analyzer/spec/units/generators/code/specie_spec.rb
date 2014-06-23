afb57b9204b2b3ba13da70b2e7affa965fd0889d
Gleb Averchuk <altermn@gmail.com>
2014-06-23 12:18:16 +0400

    add #enum_name method to specie code generator

--- analyzer/spec/units/generators/code/specie_spec.rb
+++ analyzer/spec/units/generators/code/specie_spec.rb
@@ -29,8 +29,30 @@ module VersatileDiamond
           end
 
           it_behaves_like :check_class_name do
-            let(:concept_spec) { double(name: :'ethane(c1: *, c1: *)') }
-            let(:class_name) { 'EthaneC1ss' }
+            let(:concept_spec) { double(name: :'extra_ethane(c1: *, c1: *)') }
+            let(:class_name) { 'ExtraEthaneC1ss' }
+          end
+        end
+
+        describe '#enum_name' do
+          shared_examples_for :check_enum_name do
+            subject { described_class.new(empty_generator, concept_spec) }
+            it { expect(subject.enum_name).to eq(enum_name) }
+          end
+
+          it_behaves_like :check_enum_name do
+            let(:concept_spec) { bridge_base }
+            let(:enum_name) { 'BRIDGE' }
+          end
+
+          it_behaves_like :check_enum_name do
+            let(:concept_spec) { activated_incoherent_bridge }
+            let(:enum_name) { 'BRIDGE_CTsi' }
+          end
+
+          it_behaves_like :check_enum_name do
+            let(:concept_spec) { double(name: :'bridge_with_dimer(ctb: *)') }
+            let(:enum_name) { 'BRIDGE_WITH_DIMER_CTBs' }
           end
         end
       end
