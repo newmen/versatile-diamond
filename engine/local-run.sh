1b480d579bac13d2328fb5cf30dba3958d0bd48c
Gleb Averchuk <altermn@gmail.com>
2014-06-27 11:05:24 +0400

    copy config files to result dir

--- engine/local-run.sh
+++ engine/local-run.sh
@@ -7,8 +7,10 @@ results_dir=${current_dir}/results/${composit_name}
 engine_bin=${current_dir}/engine
 tmp_dir=/tmp/vd-engine-${composit_name}
 
+mkdir -p $results_dir
+cp ${current_dir}/configs/* ${results_dir}/
+
 mkdir -p $tmp_dir
 $engine_bin ${tmp_dir}/${2} $3 $4 $5 $6 $7 > ${tmp_dir}/log
 
-mkdir -p $results_dir
 mv ${tmp_dir}/* ${results_dir}/
