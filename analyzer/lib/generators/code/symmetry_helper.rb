af2829640c6bd92a5a0456f30880f9fb024ab064
Gleb Averchuk <altermn@gmail.com>
2014-08-27 11:30:43 +0400

    erase symmetry find logic from atom sequence

---
+++ analyzer/lib/generators/code/symmetry_helper.rb
@@ -0,0 +1,39 @@
+module VersatileDiamond
+  module Generators
+    module Code
+
+      # Provides useful methods for specie analysing
+      module SymmetryHelper
+      protected
+
+        # Gets anchors of internal specie
+        # @return [Array] the array of anchor atoms
+        def anchors
+          spec.target.links.keys
+        end
+
+        # Gets the all atoms of internal specie
+        # @return [Array] the array of atoms
+        def atoms
+          spec.links.keys
+        end
+
+      private
+
+        # Wraps dependent spec to atom sequence instance
+        # @param [Organizers::DependentWrappedSpec] spec which will be wrapped
+        # @return [AtomSequence] the instance with passed specie
+        def get(spec)
+          cacher.get(spec)
+        end
+
+        # Gets sorted parents of target specie
+        # @return [Array] the sorted array of parent seqeucnes
+        def sorted_parents
+          spec.parents.sort_by { |p| -p.relations_num }
+        end
+      end
+
+    end
+  end
+end
