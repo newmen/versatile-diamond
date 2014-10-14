db75a3e89b1688b8bcd826c97b37a3626b60fefb
Gleb Averchuk <altermn@gmail.com>
2014-10-15 00:25:53 +0400

    add missed tests for dependent wrapped spec

--- analyzer/spec/units/organizers/dependent_wrapped_spec_spec.rb
+++ analyzer/spec/units/organizers/dependent_wrapped_spec_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module VersatileDiamond
   module Organizers
 
-    describe DependentWrappedSpec do
+    describe DependentWrappedSpec, type: :organizer do
       subject { described_class.new(bridge_base) }
 
       describe '#initialize' do
@@ -19,6 +19,53 @@ module VersatileDiamond
 
       describe '#external_bonds' do
         it { expect(subject.external_bonds).to eq(subject.spec.external_bonds) }
+      end
+
+      describe '#anchors' do
+        it { expect(subject.anchors).to match_array(bridge_base.links.keys) }
+      end
+
+      describe '#sorted_parents' do
+        let(:sorted_parents) { [dept_methyl_on_bridge_base, dept_bridge_base] }
+        before do
+          sorted_parents.shuffle.each do |parent|
+            dept_methyl_on_dimer_base.store_parent(parent)
+          end
+        end
+        it { expect(dept_methyl_on_dimer_base.sorted_parents).to eq(sorted_parents) }
+      end
+
+      describe 'dependency tree position properties' do
+        shared_examples_for :store_one_parent_before do
+          before { dept_methyl_on_bridge_base.store_parent(dept_bridge_base) }
+        end
+
+        shared_examples_for :store_many_parent_before do
+          before do
+            dept_methyl_on_dimer_base.store_parent(dept_bridge_base)
+            dept_methyl_on_dimer_base.store_parent(dept_methyl_on_bridge_base)
+          end
+        end
+
+        describe '#source?' do
+          it { expect(dept_bridge_base.source?).to be_truthy }
+          it_behaves_like :store_one_parent_before do
+            it { expect(dept_methyl_on_bridge_base.source?).to be_falsey }
+          end
+          it_behaves_like :store_many_parent_before do
+            it { expect(dept_methyl_on_dimer_base.source?).to be_falsey }
+          end
+        end
+
+        describe '#complex?' do
+          it { expect(dept_bridge_base.complex?).to be_falsey }
+          it_behaves_like :store_one_parent_before do
+            it { expect(dept_methyl_on_bridge_base.complex?).to be_falsey }
+          end
+          it_behaves_like :store_many_parent_before do
+            it { expect(dept_methyl_on_dimer_base.complex?).to be_truthy }
+          end
+        end
       end
     end
 
