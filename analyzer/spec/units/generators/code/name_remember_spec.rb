b5fc0ac86df267a31d650e4d3aea2f502c63e10d
Gleb Averchuk <altermn@gmail.com>
2014-10-16 05:14:14 +0400

    modify NameRemember behavior, now it works identically for single object variable or for collection of variables

--- analyzer/spec/units/generators/code/name_remember_spec.rb
+++ analyzer/spec/units/generators/code/name_remember_spec.rb
@@ -8,23 +8,38 @@ module VersatileDiamond
         subject { described_class.new }
 
         describe '#assign' do
-          it 'no raise error' do
-            expect { subject.assign('var', ['value']) }.not_to raise_error
+          describe 'no raise error' do
+            it { expect { subject.assign('var', 'value') }.not_to raise_error }
+            it { expect { subject.assign('var', ['value']) }.not_to raise_error }
           end
 
-          it 'empty variables array' do
-            expect { subject.assign('var', []) }.to raise_error
+          describe 'duplicate variable' do
+            shared_examples_for :check_var_duplicate do
+              before { subject.assign('var', vars) }
+              it { expect { subject.assign('other', vars) }.to raise_error }
+            end
+
+            it_behaves_like :check_var_duplicate do
+              let(:vars) { 'value' }
+            end
+
+            it_behaves_like :check_var_duplicate do
+              let(:vars) { ['value'] }
+            end
           end
 
-          it 'duplicate variable' do
-            var = 'value'
-            subject.assign('var', [var])
-            expect { subject.assign('other', [var]) }.to raise_error
-          end
+          describe 'duplicate name' do
+            shared_examples_for :check_name_duplicate do
+              before { subject.assign('var', var1) }
+              it { expect { subject.assign('var', var2) }.to raise_error }
+            end
 
-          it 'duplicate name' do
-            subject.assign('var', ['value'])
-            expect { subject.assign('var', ['other']) }.to raise_error
+            ['a', ['b']].product(['c', ['d']]).each do |var1, var2|
+              it_behaves_like :check_name_duplicate do
+                let(:var1) { var1 }
+                let(:var2) { var2 }
+              end
+            end
           end
         end
 
@@ -32,7 +47,7 @@ module VersatileDiamond
           describe 'with number one' do
             let(:var) { 'value' }
             before { subject.assign_next('var', var) }
-            it { expect(subject.get(var)).to eq('var1') }
+            it { expect(subject.name_of(var)).to eq('var1') }
           end
 
           describe 'with number two' do
@@ -42,7 +57,7 @@ module VersatileDiamond
               subject.assign_next('var', var1)
               subject.assign_next('var', var2)
             end
-            it { expect(subject.get(var2)).to eq('var2') }
+            it { expect(subject.name_of(var2)).to eq('var2') }
           end
 
           describe 'duplicate variable' do
@@ -60,56 +75,71 @@ module VersatileDiamond
         end
 
         describe '#reassign' do
-          before do
-            subject.assign('var', [1])
-            subject.reassign('var', [2])
+          shared_examples_for :check_reassign do
+            before do
+              subject.assign('var', var1)
+              subject.reassign('var', var2)
+            end
+
+            it { expect(subject.name_of(1)).to be_nil }
+            it { expect(subject.name_of(2)).to eq('var') }
           end
 
-          it { expect(subject.get(2)).to eq('var') }
-          it 'not available by old value' do
-            expect { subject.get(1) }.to raise_error
-          end
-        end
-
-        describe '#get' do
-          describe 'single variable' do
-            let(:var) { 123 }
-            before { subject.assign('var', [var]) }
-            it { expect(subject.get(var)).to eq('var') }
-          end
-
-          describe 'many variables' do
-            let(:vars) { [1, 2, 3] }
-            before { subject.assign('var', vars) }
-            it { expect(subject.get(2)).to eq('vars[1]') }
-          end
-
-          it 'undefined variable' do
-            expect { subject.get('wrong') }.to raise_error
+          [1, [1]].product([2, [2]]) do |var1, var2|
+            it_behaves_like :check_reassign do
+              let(:var1) { var1 }
+              let(:var2) { var2 }
+            end
           end
         end
 
-        describe '#array_name_for' do
-          before { subject.assign('var', vars) }
+        describe '#name_of' do
+          describe 'argument is object' do
+            describe 'single variable' do
+              let(:var) { 123 }
+              before { subject.assign('var', var) }
+              it { expect(subject.name_of(var)).to eq('var') }
+            end
 
-          describe 'all is correct' do
-            let(:vars) { ['a', 'b', 'c'] }
-            it { expect(subject.array_name_for(vars)).to eq('vars') }
-          end
+            describe 'many variables' do
+              let(:vars) { [1, 2, 3] }
+              before { subject.assign('var', vars) }
+              it { expect(subject.name_of(2)).to eq('vars[1]') }
+            end
 
-          describe 'incorrect vars array' do
-            let(:vars) { ['a', 'b'] }
-            it 'variable is excess' do
-              excess_vars = ['c']
-              subject.assign('other', excess_vars)
-              expect { subject.array_name_for(vars + excess_vars) }.to raise_error
+            it 'undefined variable' do
+              expect(subject.name_of('wrong')).to be_nil
             end
           end
 
-          describe 'vars with one item' do
-            let(:vars) { ['a'] }
-            it 'variable is excess' do
-              expect { subject.array_name_for(vars) }.to raise_error
+          describe 'argument is array' do
+            describe 'all is correct' do
+              let(:vars) { ['a', 'b', 'c'] }
+              before { subject.assign('var', vars) }
+              it { expect(subject.name_of(vars)).to eq('vars') }
+            end
+
+            describe 'incorrect vars set' do
+              let(:vars) { ['a', 'b'] }
+              let(:excess_var) { 'c' }
+
+              shared_examples_for :check_error_raising do
+                before { subject.assign('var', vars) }
+                it { expect { subject.name_of(vars + [excess_var]) }.to raise_error }
+              end
+
+              describe 'excess vars' do
+                before { subject.assign('other', excess_var) }
+                it_behaves_like :check_error_raising
+              end
+
+              describe 'not all vars is prestented' do
+                it_behaves_like :check_error_raising
+              end
+            end
+
+            it 'undefined variable' do
+              expect(subject.name_of(['not', 'defined', 'array'])).to be_nil
             end
           end
         end
@@ -118,26 +148,14 @@ module VersatileDiamond
           let(:vars) { [1, 2] }
           let(:other) { :other }
           before do
-            subject.assign('other', [other])
+            subject.assign('other', other)
             subject.assign('var', vars)
             subject.erase(vars)
           end
-          it { expect(subject.assigned?(vars.first)).to be_falsey }
-          it { expect(subject.assigned?(vars.last)).to be_falsey }
-          it { expect(subject.assigned?(other)).to be_truthy }
-        end
 
-        describe '#assigned?' do
-          let(:var) { :value }
-
-          describe 'yes' do
-            before { subject.assign('var', [var]) }
-            it { expect(subject.assigned?(var)).to be_truthy }
-          end
-
-          describe 'no' do
-            it { expect(subject.assigned?(var)).to be_falsey }
-          end
+          it { expect(subject.name_of(vars.first)).to be_nil }
+          it { expect(subject.name_of(vars.last)).to be_nil }
+          it { expect(subject.name_of(other)).not_to be_nil }
         end
       end
 
