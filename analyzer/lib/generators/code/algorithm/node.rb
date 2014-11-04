50728059f8dbe31d9452eec5ff4104fe7533417f
Gleb Averchuk <altermn@gmail.com>
2014-11-04 17:11:40 +0300

    a bit of comments

--- analyzer/lib/generators/code/algorithm/node.rb
+++ analyzer/lib/generators/code/algorithm/node.rb
@@ -3,7 +3,7 @@ module VersatileDiamond
     module Code
       module Algorithm
 
-        # The AST node for generation cpp code
+        # The value for AST tree which uses for generation cpp code
         class Node
           include Modules::OrderProvider
           extend Forwardable
@@ -11,12 +11,24 @@ module VersatileDiamond
           attr_reader :original_specie, :uniq_specie, :atom
           def_delegators :uniq_specie, :none?, :scope?
 
+          # Initializes the node object
+          # @param [Specie] original_specie which (or which atom) was plased in
+          #   original analysing graph vertex
+          # @param [NoneSpec | UniqueSpecie | SpeciesScope] uniq_specie which
+          #   correspond to using parent species
+          # @param [Concepts::Atom | Concepts::AtomRelation | Concepts::SpecificAtom]
+          #   atom which was a vertex of original graph
           def initialize(original_specie, uniq_specie, atom)
             @original_specie = original_specie
             @uniq_specie = uniq_specie
             @atom = atom
           end
 
+          # Compares current node with another node. At the beginning of sequence puts
+          # the biggest nodes.
+          #
+          # @param [Node] other comparing node
+          # @return [Integer] the comparing result
           def <=> (other)
             typed_order(uniq_specie, other.uniq_specie, :none?) do
               typed_order(other.uniq_specie, uniq_specie, :scope?) do
@@ -27,6 +39,8 @@ module VersatileDiamond
             end
           end
 
+          # Directly provides atom properties instance for current node
+          # @return [Organizers::AtomProperties] for instances that stored in node
           def properties
             Organizers::AtomProperties.new(@original_specie.spec, @atom)
           end
--- analyzer/spec/support/generators/code/algorithm/nodes_converter.rb
+++ analyzer/spec/support/generators/code/algorithm/nodes_converter.rb
@@ -7,14 +7,25 @@ module VersatileDiamond
           # Provides usefull methods for working with noded graphs
           module NodesConverter
 
+            # Provides simple array with two element instead of the node
+            # @param [Node] node which will be converted
+            # @return [Array] the simple checkable array with two values
             def typed_node(node)
               [node.uniq_specie.class, node.atom]
             end
 
+            # Collects the list of unique nodes which used in passed graph
+            # @param [Hash] node_graph from which the list will be collected
+            # @return [Array] the list of converted nodes
             def typed_nodes_list(nodes_graph)
               nodes_graph.keys.flatten.map(&method(:typed_node)).uniq
             end
 
+            # Translates the passed graph to another graph where instead nodes the
+            # atoms uses as vertices
+            #
+            # @param [Hash] node_graph which will be translated
+            # @return [Hash] translated atomic graph
             def translate_to_atomic_graph(nodes_graph)
               nodes_graph.each_with_object({}) do |(nodes, rels), acc|
                 acc[nodes.map(&:atom)] = rels.map { |ns, r| [ns.map(&:atom), r] }
