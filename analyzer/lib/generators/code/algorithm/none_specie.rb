1375297f056999f1c608e295626915d7327bae4d
Gleb Averchuk <altermn@gmail.com>
2014-11-06 14:50:51 +0300

    add rspecs for algorithm species

--- analyzer/lib/generators/code/algorithm/none_specie.rb
+++ analyzer/lib/generators/code/algorithm/none_specie.rb
@@ -5,15 +5,20 @@ module VersatileDiamond
 
         # Provides specie which means "no specie"
         class NoneSpecie
-
+          # Initialize "no specie" by original specie
+          # @param [Specie] specie which will be remembered
           def initialize(specie)
             @specie = specie
           end
 
+          # "No specie" is always "no specie"
+          # @return [Boolean] true
           def none?
             true
           end
 
+          # "No specie" is not scope
+          # @return [Boolean] false
           def scope?
             false
           end
