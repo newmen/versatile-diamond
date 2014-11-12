9d60776130c95d975e38c0c76fe1c34d5435f95a
Gleb Averchuk <altermn@gmail.com>
2014-11-12 05:09:05 +0300

    add base test cases to reaction find builder rspec

--- analyzer/spec/units/generators/code/algorithm/reaction_find_builder_spec.rb
+++ analyzer/spec/units/generators/code/algorithm/reaction_find_builder_spec.rb
@@ -9,9 +9,13 @@ module VersatileDiamond
           let(:base_specs) { [] }
           let(:specific_specs) { [] }
           let(:generator) do
+            bases = base_specs.dup
+            specifics = specific_specs.dup
+            (target_spec.specific? ? specifics : bases) << target_spec
+
             stub_generator(
-              base_specs: base_specs,
-              specific_specs: specific_specs,
+              base_specs: bases,
+              specific_specs: specifics,
               typical_reactions: [subject])
           end
 
@@ -20,6 +24,149 @@ module VersatileDiamond
           let(:builder) { described_class.new(generator, code_reaction, code_specie) }
 
           describe '#build' do
+            Support::RoleChecker::ANCHOR_KEYNAMES.each do |keyname|
+              let(:"other_role_#{keyname}") { role(other_spec, keyname) }
+            end
+
+            it_behaves_like :check_code do
+              subject { dept_methyl_activation }
+              let(:target_spec) { dept_methyl_on_bridge_base }
+              let(:find_algorithm) do
+                <<-CODE
+    create<ForwardMethylActivation>(target);
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              subject { dept_methyl_desorption }
+              let(:target_spec) { dept_methyl_on_bridge_base }
+              let(:find_algorithm) do
+                <<-CODE
+    create<ForwardMethylDesorption>(target);
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              subject { dept_sierpinski_drop }
+              let(:target_spec) { dept_cross_bridge_on_bridges_base }
+              let(:find_algorithm) do
+                <<-CODE
+    target->eachSymmetry([](SpecificSpec *specie) {
+        create<ForwardSierpinskiDrop>(specie);
+    });
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              subject { dept_sierpinski_formation }
+              let(:target_spec) { dept_activated_methyl_on_bridge }
+              let(:other_spec) { dept_activated_bridge }
+              let(:find_algorithm) do
+                <<-CODE
+    Atom *anchor = target->atom(0);
+    eachNeighbour(anchor, &Diamond::cross_100, [target](Atom *neighbour) {
+        if (neighbour->is(#{role_cb}))
+        {
+            SpecificSpec *targets[2] = { target, neighbour->specByRole<BridgeCTs>(#{other_role_ct}) };
+            create<ReverseSierpinskiDrop>(targets);
+        }
+    });
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              subject { dept_dimer_formation }
+              let(:target_spec) { dept_activated_incoherent_bridge }
+              let(:other_spec) { dept_activated_bridge }
+              let(:find_algorithm) do
+                <<-CODE
+    Atom *anchor = target->atom(0);
+    eachNeighbour(anchor, &Diamond::front_100, [target](Atom *neighbour) {
+        if (neighbour->is(#{role_ct}))
+        {
+            SpecificSpec *targets[2] = { target, neighbour->specByRole<BridgeCTs>(#{other_role_ct}) };
+            create<ForwardDimerFormation>(targets);
+        }
+    });
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              subject { dept_dimer_formation }
+              let(:target_spec) { dept_activated_bridge }
+              let(:other_spec) { dept_activated_incoherent_bridge }
+              let(:find_algorithm) do
+                <<-CODE
+    Atom *anchor = target->atom(0);
+    eachNeighbour(anchor, &Diamond::front_100, [target](Atom *neighbour) {
+        if (neighbour->is(#{role_ct}))
+        {
+            SpecificSpec *targets[2] = { neighbour->specByRole<BridgeCTsi>(#{other_role_ct}), target };
+            create<ForwardDimerFormation>(targets);
+        }
+    });
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              let(:base_specs) { [dept_bridge_base, dept_dimer_base] }
+              subject { dept_methyl_incorporation }
+              let(:target_spec) { dept_activated_methyl_on_bridge }
+              let(:other_spec) { dept_activated_dimer }
+
+              let(:find_algorithm) do
+                <<-CODE
+    Atom *anchors[2] = { target->atom(2), target->atom(3) };
+    eachNeighbours<2>(anchors, &Diamond::cross_100, [target](Atom **neighbours) {
+        if (neighbours[0]->is(#{other_role_cl}) && neighbours[1]->is(#{other_role_cl}) && neighbours[0]->hasBondWith(neighbours[1]))
+        {
+            for (int i = 0; i < 2; ++i)
+            {
+                if (neighbours[i]->is(#{other_role_cr}))
+                {
+                    SpecificSpec *targets[2] = { neighbours[i]->specByRole<DimerCRs>(#{other_role_cr}), target };
+                    create<ForwardMethylIncorporation>(targets);
+                }
+            }
+        }
+    });
+                CODE
+              end
+            end
+
+            it_behaves_like :check_code do
+              let(:base_specs) { [dept_bridge_base, dept_methyl_on_bridge_base] }
+              subject { dept_methyl_incorporation }
+              let(:target_spec) { dept_activated_dimer }
+              let(:other_spec) { dept_activated_methyl_on_bridge }
+
+              let(:find_algorithm) do
+                <<-CODE
+    Atom *anchors[2] = { target->atom(3), target->atom(0) };
+    eachNeighbours<2>(anchors, &Diamond::cross_100, [target](Atom **neighbours) {
+        if (neighbours[0]->is(#{other_role_cr}) && neighbours[1]->is(#{other_role_cr}))
+        {
+            Atom *atom1 = crystalBy(neighbours[0])->atom(Diamond::front_110_at(neighbours[0], neighbours[1]));
+            if (atom1 && atom1->is(#{other_role_cb}))
+            {
+                Atom *amorph1 = atom1->amorphNeighbour();
+                if (amorph1->is(#{other_role_cm}))
+                {
+                    SpecificSpec *targets[2] = { target, neighbours[i]->specByRole<MethylOnBridgeCMs>(#{other_role_cm}) };
+                    create<ForwardMethylIncorporation>(targets);
+                }
+            }
+        }
+    });
+                CODE
+              end
+            end
           end
         end
 
