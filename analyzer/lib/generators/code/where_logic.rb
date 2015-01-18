1a3153f33aef04ec10210c2c8ee4682ef4220653
Gleb Averchuk <altermn@gmail.com>
2015-01-18 12:57:13 +0300

    fix args of where logic method

--- analyzer/lib/generators/code/where_logic.rb
+++ analyzer/lib/generators/code/where_logic.rb
@@ -24,6 +24,12 @@ module VersatileDiamond
         def algorithm
         end
 
+        # Checks that where object has many target atoms
+        # @return [Boolean] has where object many target atoms or not
+        def many_targets?
+          where.links.size > 1
+        end
+
       private
 
         attr_reader :where
@@ -31,14 +37,15 @@ module VersatileDiamond
         # Gets the name of where logic method
         # @return [String] the name of where logic method
         def method_name
-          classified_str = where.description.gsub(/\s+/, '_').classify
+          classified_str = where.name.to_s.gsub(/\s+/, '_').classify
           classified_str.tap { |str| str[0] = str[0].downcase }
         end
 
         # Gets the list of arguments for where logic method
         # @return [String] the list of arguments for where logic method without brakets
         def method_args
-
+          first_arg = many_targets? ? 'Atom **atoms' : 'Atom *atom'
+          "#{first_arg}, const L &lambda"
         end
       end
 
