81795417cbf8630def8364eed876015358d04c58
Gleb Averchuk <altermn@gmail.com>
2015-05-10 00:56:23 +0300

    ln -s from hand-generations source for all common files

--- engine/hand-generations/src/reactions/single_lateral.h
+++ engine/hand-generations/src/reactions/single_lateral.h
@@ -1,15 +1,1 @@
-#ifndef SINGLE_LATERAL_H
-#define SINGLE_LATERAL_H
-
-#include "lateral.h"
-
-template <ushort RT, ushort LATERALS_NUM>
-class SingleLateral : public Lateral<ConcreteLateralReaction<LATERALS_NUM>, RT>
-{
-    typedef Lateral<ConcreteLateralReaction<LATERALS_NUM>, RT> ParentType;
-
-protected:
-    template <class... Args> SingleLateral(Args... args) : ParentType(args...) {}
-};
-
-#endif // SINGLE_LATERAL_H
+../../../../analyzer/lib/generators/code/common_files/reactions/single_lateral.h
--- engine/hand-generations/src/reactions/typical.h
+++ engine/hand-generations/src/reactions/typical.h
@@ -1,16 +1,1 @@
-#ifndef TYPICAL_H
-#define TYPICAL_H
-
-#include "registrator.h"
-
-template <ushort RT, ushort TARGETS_NUM = 1>
-class Typical : public Registrator<ConcreteTypicalReaction<TypicalReaction, TARGETS_NUM>, RT>
-{
-public:
-    typedef Registrator<ConcreteTypicalReaction<TypicalReaction, TARGETS_NUM>, RT> RegistratorType;
-
-protected:
-    template <class... Args> Typical(Args... args) : RegistratorType(args...) {}
-};
-
-#endif // TYPICAL_H
+../../../../analyzer/lib/generators/code/common_files/reactions/typical.h
