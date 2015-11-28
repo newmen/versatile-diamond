6d509e6b8458ccb1a6a6178db1d8837455f06154
Gleb Averchuk <altermn@gmail.com>
2015-11-28 04:29:30 +0300

    fix counting crytal relations in atom properties

--- analyzer/lib/organizers/atom_properties.rb
+++ analyzer/lib/organizers/atom_properties.rb
@@ -555,7 +555,7 @@ module VersatileDiamond
       # Gets number of relations which belongs to crystal
       # @return [Integer] the number of crystal relations
       def crystal_relatons_num
-        relations.select(&:belongs_to_crystal?).size
+        relations.reject { |r| r.class == Symbol }.select(&:belongs_to_crystal?).size
       end
 
       # Gets number of established and dangling bond relations
