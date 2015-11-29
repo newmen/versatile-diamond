00bb53ae6a39a5e7a3e9e6e036bdf5a602024b42
Gleb Averchuk <altermn@gmail.com>
2015-11-29 10:48:14 +0300

    upgrade transitive matrix for possible list of binary operations

--- analyzer/lib/concepts/veiled_instance.rb
+++ analyzer/lib/concepts/veiled_instance.rb
@@ -3,16 +3,8 @@ module VersatileDiamond
 
     # Uses for replasing similar concept instances
     class VeiledInstance < Tools::TransparentProxy
-      # Compares with other veiled or original instance
-      # @param [Object] other with which the current instance will be compared
-      # @return [Boolean] is same or not
-      def same?(other)
-        if self.class == other.class
-          original.same?(other.original)
-        else
-          original.same?(other)
-        end
-      end
+
+      binary_operations :same?
 
       def inspect
         "veiled_#{i}:#{original.inspect}"
--- analyzer/lib/generators/code/algorithm/other_side_specie.rb
+++ analyzer/lib/generators/code/algorithm/other_side_specie.rb
@@ -5,12 +5,7 @@ module VersatileDiamond
 
         # Garanties uniquality of all similar species
         class OtherSideSpecie < Tools::TransparentProxy
-          # Compares two other side species
-          # @param [OtherSideSpecie] other comparing proxy specie
-          # @return [Integer] the result of original species comparing
-          def <=> (other)
-            original <=> other.original
-          end
+          binary_operations :'<=>'
         end
 
       end
