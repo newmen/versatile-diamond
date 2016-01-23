337ed81ecd9d3aa4a32ae324d999b93aa0e00078
Gleb Averchuk <altermn@gmail.com>
2016-01-23 17:49:35 +0300

    platform specific changes of osx pointers representation

--- analyzer/lib/mcs/cpp/object_id.h
+++ analyzer/lib/mcs/cpp/object_id.h
@@ -1,9 +1,10 @@
 #ifndef OBJECT_ID_H
 #define OBJECT_ID_H
 
+#include <cstdint>
 #include "hanser_recursive.h"
 
-typedef unsigned int ObjectID;
+typedef uint64_t ObjectID;
 typedef HanserRecursive<ObjectID> HR;
 
 #endif // OBJECT_ID_H
