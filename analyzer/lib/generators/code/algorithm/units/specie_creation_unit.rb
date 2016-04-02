95fe9d6fc122a589400c68d617fbe2d2f8998fb1
Gleb Averchuk <altermn@gmail.com>
2016-04-02 08:45:34 +0300

    add specie creation unit

---
+++ analyzer/lib/generators/code/algorithm/units/specie_creation_unit.rb
@@ -0,0 +1,119 @@
+module VersatileDiamond
+  module Generators
+    module Code
+      module Algorithm::Units
+
+        # The unit for combines statements of specie creation
+        class SpecieCreationUnit < GenerableUnit
+          # @param [Expressions::VarsDictionary] dict
+          # @param [BaseContext] context
+          # @param [Specie] specie
+          def initialize(dict, context, specie)
+            super(dict)
+            @specie = specie
+
+            @major_atoms = @specie.sequence.short
+            @addition_atoms = @specie.sequence.addition_atoms
+            @parent_species =
+              context.bone_nodes.map(&:uniq_specie).reject(&:none?).uniq.sort
+          end
+
+          # @return [Expressions::Core::Statement]
+          def create
+            if !@parent_species.empty? && all_defined?(@parent_species)
+              create_from_parent_species
+            elsif all_defined?(@major_atoms)
+              create_from_major_atoms
+            else
+              raise 'Not all required entities were defined'
+            end
+          end
+
+        private
+
+          # @param [Array] atoms
+          # @yield incorporating statement
+          # @return [Expressions::Core::Statement]
+          def redefine_atoms_as_array(atoms, &block)
+            if same_arr?(atoms)
+              block.call
+            else
+              remake_atoms_as_array(atoms).define_var + block.call
+            end
+          end
+
+          # @yield incorporating statement
+          # @return [Expressions::Core::Statement]
+          def redefine_parent_species_as_array(&block)
+            if same_arr?(@parent_species)
+              block.call
+            else
+              remake_parent_species_as_array.define_var + block.call
+            end
+          end
+
+          # @return [Expressions::Core::Statement]
+          def create_from_major_atoms
+            redefine_atoms_as_array(@major_atoms) do
+              call_create(dict.var_of(@major_atoms))
+            end
+          end
+
+          # @return [Expressions::Core::Statement]
+          def create_from_parent_species
+            redefine_parent_species_as_array do
+              if @addition_atoms.empty?
+                create_with_parent_species
+              else
+                create_with_additional_atoms
+              end
+            end
+          end
+
+          # @return [Expressions::Core::FunctionCall]
+          def create_with_parent_species
+            call_create(dict.var_of(@parent_species))
+          end
+
+          # @return [Expressions::Core::Statement]
+          def create_with_additional_atoms
+            redefine_atoms_as_array(@addition_atoms) do
+              call_create(dict.var_of(@addition_atoms), dict.var_of(@parent_species))
+            end
+          end
+
+          # @param [Array] atoms
+          # @return [Expressions::Core::Collection]
+          def remake_atoms_as_array(atoms)
+            dict.make_atom_s(atoms, value: vars_for(atoms))
+          end
+
+          # @return [Expressions::Core::Collection]
+          def remake_parent_species_as_array
+            type = Expressions::ParentSpecieType[]
+            values = vars_for(@parent_species)
+            dict.make_specie_s(@parent_species, type: type, value: values)
+          end
+
+          # @return [Expressions::Core::FunctionCall]
+          def call_create(*exprs)
+            type = Expressions::Core::ObjectType[@specie.class_name]
+            Expressions::Core::FunctionCall['create', *exprs, template_args: [type]]
+          end
+
+          # @param [Array] instances
+          # @return [Boolean]
+          def same_arr?(instances)
+            if instances.one?
+              true
+            else
+              arr = dict.var_of(instances)
+              arr && arr.items.map(&:instance) == instances # same order
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
---
+++ analyzer/spec/units/generators/code/algorithm/units/specie_creation_unit_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+module VersatileDiamond
+  module Generators
+    module Code
+      module Algorithm::Units
+
+        describe SpecieCreationUnit, type: :algorithm do
+          subject { described_class.new(dict, context, specie) }
+          let(:context) { SpecieContext.new(dict, backbone.big_graph, ordered_graph) }
+          let(:specie) { generator.specie_class(dept_uniq_specie.name) }
+          let(:bone_nodes) { context.bone_nodes }
+          let(:addition_atoms) { specie.sequence.addition_atoms }
+
+          describe '#create' do
+            describe 'from major atoms which already arr' do
+              include_context :bridge_context
+              before { dict.make_atom_s(bone_nodes.map(&:atom)) }
+              it { expect(subject.create.code).to eq('create<Bridge>(atoms1)') }
+            end
+
+            describe 'from major atoms not same arr' do
+              include_context :bridge_context
+              before do
+                first, *nodes = bone_nodes
+                dict.make_atom_s(first.atom)
+                dict.make_atom_s(nodes.map(&:atom))
+              end
+              let(:code) do
+                <<-CODE
+Atom *atoms2[3] = { atom1, atoms1[0], atoms1[1] };
+create<Bridge>(atoms2);
+                CODE
+              end
+              it { expect(subject.create.code).to eq(code.rstrip) }
+            end
+
+            describe 'from parent species' do
+              let(:parent_species) do
+                bone_nodes.map(&:uniq_specie).reject(&:none?).uniq
+              end
+
+              describe 'which already arr' do
+                include_context :two_mobs_context
+                before { dict.make_specie_s(parent_species) }
+                let(:code) { 'create<CrossBridgeOnBridges>(species1)' }
+                it { expect(subject.create.code).to eq(code) }
+              end
+
+              describe 'not same arr' do
+                include_context :two_mobs_context
+                before { parent_species.map(&dict.public_method(:make_specie_s)) }
+                let(:code) do
+                  <<-CODE
+ParentSpec *species1[2] = { methylOnBridge1, methylOnBridge2 };
+create<CrossBridgeOnBridges>(species1);
+                  CODE
+                end
+                it { expect(subject.create.code).to eq(code.rstrip) }
+              end
+
+              describe 'with one additional atom' do
+                include_context :mob_context
+                before do
+                  dict.make_atom_s(addition_atoms)
+                  dict.make_specie_s(parent_species)
+                end
+                let(:code) { 'create<MethylOnBridge>(amorph1, bridge1)' }
+                it { expect(subject.create.code).to eq(code) }
+              end
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
