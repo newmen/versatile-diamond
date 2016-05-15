ba1a95b2050f9b28b14260161e1a938a16a78d47
Gleb Averchuk <altermn@gmail.com>
2016-05-15 05:44:27 +0300

    fix first test of two side dimers formation find algorithm

--- analyzer/spec/functional/generators/code/algorithm/reaction_find_builder_spec.rb
+++ analyzer/spec/functional/generators/code/algorithm/reaction_find_builder_spec.rb
@@ -507,30 +507,34 @@ module VersatileDiamond
                 let(:thrid_spec) { dept_extra_activated_methyl_on_bridge }
                 let(:find_algorithm) do
                   <<-CODE
-    Atom *anchor = target->atom(2);
-    eachNeighbour(anchor, &Diamond::front_100, [&](Atom *neighbour1) {
+    Atom *atom1 = target->atom(2);
+    eachNeighbour(atom1, &Diamond::front_100, [&](Atom *neighbour1) {
         if (neighbour1->is(#{other_role_cl}))
         {
-            DimerCLsCRi *specie1 = neighbour1->specByRole<DimerCLsCRi>(#{other_role_cl});
-            Atom *anchors1[2] = { neighbour1, anchor };
-            eachNeighbours<2>(anchors1, &Diamond::cross_100, [&](Atom **neighbours1) {
+            DimerCLsCRi *dimerCLsCRi1 = neighbour1->specByRole<DimerCLsCRi>(#{other_role_cl});
+            Atom *atoms1[2] = { neighbour1, atom1 };
+            eachNeighbours<2>(atoms1, &Diamond::cross_100, [&dimerCLsCRi1, &target](Atom **neighbours1) {
                 if (neighbours1[0]->is(#{thrid_role_cr}) && neighbours1[1]->is(#{thrid_role_cr}))
                 {
-                    neighbourFrom(neighbours1, &Diamond::front_110_at, [&](Atom *neighbour2) {
-                        if (neighbour2->is(#{thrid_role_cb}) && neighbours1[0]->hasBondWith(neighbour2) && neighbours1[1]->hasBondWith(neighbour2))
+                    neighbourFrom(neighbours1, &Diamond::front_110_at, [&dimerCLsCRi1, &neighbours1, &target](Atom *neighbour2) {
+                        if (neighbour2->is(#{thrid_role_cb}))
                         {
-                            neighbour2->eachAmorphNeighbour([&](Atom *amorph1) {
-                                if (amorph1->is(#{thrid_role_cm}))
-                                {
-                                    MethylOnBridgeCMss *specie2 = amorph1->specByRole<MethylOnBridgeCMss>(#{thrid_role_cm});
-                                    specie2->eachSymmetry([](SpecificSpec *specie3) {
-                                    if (specie3->atom(2) == neighbours1[1] && specie3->atom(3) == neighbours1[0])
+                            if (neighbours1[0]->hasBondWith(neighbour2) && neighbours1[1]->hasBondWith(neighbour2))
+                            {
+                                neighbour2->eachAmorphNeighbour([&dimerCLsCRi1, &neighbours1, &target](Atom *amorph1) {
+                                    if (amorph1->is(#{thrid_role_cm}))
                                     {
-                                        SpecificSpec *targets[3] = { specie1, specie3, target };
-                                        create<ForwardTwoSideDimersForm>(targets);
+                                        MethylOnBridgeCMss *methylOnBridgeCMss1 = amorph1->specByRole<MethylOnBridgeCMss>(#{thrid_role_cm});
+                                        methylOnBridgeCMss1->eachSymmetry([&dimerCLsCRi1, &neighbours1, &target](SpecificSpec *symmetricMethylOnBridgeCMss1) {
+                                            if (neighbours1[0] == symmetricMethylOnBridgeCMss1->atom(2) && neighbours1[1] == symmetricMethylOnBridgeCMss1->atom(3))
+                                            {
+                                                SpecificSpec *targets[3] = { dimerCLsCRi1, symmetricMethylOnBridgeCMss1, target };
+                                                create<ForwardTwoSideDimersForm>(targets);
+                                            }
+                                        });
                                     }
-                                }
-                            });
+                                });
+                            }
                         }
                     });
                 }
